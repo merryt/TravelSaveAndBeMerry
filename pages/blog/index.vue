bac2c0c75d19b63a46d47ef0bca0f15bca56c063
Tyler Merry <tymerry@gmail.com>
2020-06-06 17:58:00 -0600

    making pretty and adding tags

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,21 +1,19 @@
 <template>
-  <article>
-    <h1>Blog Post</h1>
-    <p>BlogPosts: {{blogPosts}} </p>
-    <p>---</p>
-    <li v-for="blogPost in blogPosts">  
-        <nuxt-link :to="blogPost.slug">{{ blogPost.title }}</nuxt-link>
-    </li>
-  </article>
+    <article>
+        <h1>Blog Post</h1>
+        <li v-for="blogPost in blogPosts" :key="blogPost.slug">
+            <nuxt-link :to="blogPost.slug">{{ blogPost.title }}</nuxt-link>
+        </li>
+    </article>
 </template>
 
 <script>
 export default {
-  computed: {
-    blogPosts() {
-      console.log(this.$store.state.blogPosts)
-      return this.$store.state.blogPosts;
-    },
-  },
-};
+    computed: {
+        blogPosts() {
+            console.log(this.$store.state.blogPosts)
+            return this.$store.state.blogPosts
+        }
+    }
+}
 </script>
--- store/index.js
+++ store/index.js
@@ -1,21 +1,25 @@
 export const state = () => ({
-    blogPosts: [],
+    blogPosts: []
 })
 
 export const mutations = {
-    setBlogPosts(state, list){
+    setBlogPosts(state, list) {
         state.blogPosts = list
     }
 }
 
 export const actions = {
-    async nuxtServerInit({ commit }){
-        let files = await require.context('~/assets/content/blog/', false, /\.json$/);
-        let blogPosts = files.keys().map(key => {
-            let res = files(key);
-            res.slug = key.slice(2, -5);
-            return res;
-        });
+    async nuxtServerInit({ commit }) {
+        const files = await require.context(
+            '~/assets/content/blog/',
+            false,
+            /\.json$/
+        )
+        const blogPosts = files.keys().map((key) => {
+            const res = files(key)
+            res.slug = key.slice(2, -5)
+            return res
+        })
         await commit('setBlogPosts', blogPosts)
     }
 }
