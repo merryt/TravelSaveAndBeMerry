6f73599e8d91063d7eedb01ff7be356666dbcbb4
Tyler Merry <tymerry@gmail.com>
2025-08-29 11:29:25 +0200

    updating ecap to not use js

--- src/lib/components/Ecap.svelte
+++ src/lib/components/Ecap.svelte
@@ -1,78 +1,38 @@
 <div class="ecap-container">
-	<!-- Begin Mailchimp Signup Form -->
-	<link
-		href="//cdn-images.mailchimp.com/embedcode/classic-10_7_dtp.css"
-		rel="stylesheet"
-		type="text/css"
-	/>
-	<h2>Sign up for our mailing list!</h2>
-	<div id="mc_embed_signup">
-		<form
-			action="https://travelsaveandbemerry.us20.list-manage.com/subscribe/post?u=6295ef881a181db0a3aabb81b&amp;id=d8b406e59a"
-			method="post"
-			id="mc-embedded-subscribe-form"
-			name="mc-embedded-subscribe-form"
-			class="validate"
-			target="_blank"
-			novalidate
-		>
-			<div id="mc_embed_signup_scroll">
-				<div class="mc-field-group">
-					<label for="mce-EMAIL">Email Address <span class="asterisk">*</span> </label>
-					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" />
-				</div>
-				<div id="mce-responses" class="clear foot">
-					<div class="response" id="mce-error-response" style="display: none" />
-					<div class="response" id="mce-success-response" style="display: none" />
-				</div>
-				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-				<div style="position: absolute; left: -5000px" aria-hidden="true">
-					<input
-						type="text"
-						name="b_6295ef881a181db0a3aabb81b_d8b406e59a"
-						tabindex="-1"
-						value=""
-					/>
-				</div>
-				<div class="optionalParent">
-					<div class="clear foot">
-						<input
-							type="submit"
-							value="Subscribe"
-							name="subscribe"
-							id="mc-embedded-subscribe"
-							class="button"
-						/>
-						<!-- <p class="brandingLogo">
-                                <a
-                                    href="http://eepurl.com/h01bFX"
-                                    title="Mailchimp - email marketing made easy and fun"
-                                    ><img
-                                        src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"
-                                /></a>
-                            </p> -->
-					</div>
-				</div>
-			</div>
-		</form>
-	</div>
-	<script
-		type="text/javascript"
-		src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
-	></script>
-	<script type="text/javascript">
-		(function ($) {
-			window.fnames = new Array();
-			window.ftypes = new Array();
-			fnames[0] = 'EMAIL';
-			ftypes[0] = 'email';
-			fnames[1] = 'FNAME';
-			ftypes[1] = 'text';
-			fnames[2] = 'LNAME';
-			ftypes[2] = 'text';
-		})(jQuery);
-		var $mcj = jQuery.noConflict(true);
-	</script>
+
+
+<div id="mc_embed_shell">
+      <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
+  <style type="text/css">
+        #mc_embed_signup{background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 600px;}
+        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+    <form action="https://travelsaveandbemerry.us20.list-manage.com/subscribe/post?u=6295ef881a181db0a3aabb81b&amp;id=d8b406e59a&amp;f_id=00af5aeef0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self" novalidate="">
+        <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
+            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+            <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></div>
+        <div id="mce-responses" class="clear foot">
+            <div class="response" id="mce-error-response" style="display: none;"></div>
+            <div class="response" id="mce-success-response" style="display: none;"></div>
+        </div>
+    <div aria-hidden="true" style="position: absolute; left: -5000px;">
+        /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
+        <input type="text" name="b_6295ef881a181db0a3aabb81b_d8b406e59a" tabindex="-1" value="">
+    </div>
+        <div class="optionalParent">
+            <div class="clear foot">
+                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe">
+                <p style="margin: 0px auto;"><a href="http://eepurl.com/jmdgjw" title="Mailchimp - email marketing made easy and fun"><span style="display: inline-block; background-color: transparent; border-radius: 4px;"><img class="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" style="width: 220px; height: 40px; display: flex; padding: 2px 0px; justify-content: center; align-items: center;"></span></a></p>
+            </div>
+        </div>
+    </div>
+</form>
+</div>
+</div>
+
+
 </div>
 
 <style>
