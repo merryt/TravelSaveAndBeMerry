549167b92822cf934a066289543734785b626121
Tyler Merry <tymerry@gmail.com>
2025-11-29 16:01:46 +0800

    resizing imasges

--- resize.mjs
+++ resize.mjs
@@ -3,12 +3,27 @@ import 'zx/globals'
 
 // this file is written with https://github.com/google/zx
 void (async function () {
-    await fs.readdirSync('./static/images/').forEach(async (element) => {
-        const width =
-            await $`identify -format "%[fx:w]" ./static/images/${element}`
-        if (width > 2000) {
-            console.log(chalk.blue(element))
-            await $`convert -resize 2000x ./static/images/${element} ./static/images/${element}`
+    const files = fs.readdirSync('./static/images/')
+    for (const element of files) {
+        if (!element.match(/\.(jpg|jpeg|png|gif|webp)$/i)) continue;
+
+        try {
+            const output = await $`identify -format "%[fx:w]" ./static/images/${element}`
+            const width = parseInt(output.stdout.trim())
+
+            if (isNaN(width)) {
+                console.warn(`Could not determine width for ${element}. Output: '${output.stdout}'`)
+                continue
+            }
+
+            console.log(`File: ${element}, Width: ${width}`)
+
+            if (width > 2000) {
+                console.log(chalk.blue(`Resizing ${element}...`))
+                await $`convert -resize 2000x ./static/images/${element} ./static/images/${element}`
+            }
+        } catch (error) {
+            console.error(`Error processing ${element}:`, error)
         }
-    })
+    }
 })()
