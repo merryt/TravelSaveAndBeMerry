5c62166bb11255926226c219c7b425afe5b61f9c
Tyler Merry <tymerry@gmail.com>
2025-12-31 14:14:28 +0700

    updating packages

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -2,8 +2,8 @@ import { getAllPosts } from "$lib/components/getAllPosts.ts";
 import type { FrontMatter } from "$lib/interfaces.ts";
 
 export async function load() {
-    const sortedPosts: Promise<{ meta: FrontMatter; path: string }[]> = getAllPosts();
+    const posts = await getAllPosts();
     return {
-        posts: sortedPosts
+        posts
     }
 }
